642a441cfa98d2ceaa7ed1e9b8bd1b21952b0c99
Maxime Thirouin <git@moox.io>
2019-08-16 17:19:37 +0200

    format

--- src/index.js
+++ src/index.js
@@ -1,26 +1,26 @@
 // @flow
 
-import React, { Component } from "react"
+import React, { Component } from "react";
 
 // topbar require window, so here is an universal workaround
 const topbar =
   typeof window === "undefined"
-  ? {
-    show: () => {},
-    hide: () => {},
-    config: () => {},
-  }
-  : require("topbar")
+    ? {
+        show: () => {},
+        hide: () => {},
+        config: () => {}
+      }
+    : require("topbar");
 
-let semaphore: number = 0
+let semaphore: number = 0;
 
 type Props = {
   topbar?: typeof topbar
-}
+};
 
 const getTopBar = (props: Props): typeof topbar => {
-  return props.topbar || topbar
-}
+  return props.topbar || topbar;
+};
 
 class TopBar extends Component<Props> {
   props: Props;
@@ -29,21 +29,21 @@ class TopBar extends Component<Props> {
 
   componentDidMount() {
     if (semaphore === 0) {
-      getTopBar(this.props).show()
+      getTopBar(this.props).show();
     }
-    semaphore++
+    semaphore++;
   }
 
   componentWillUnmount() {
-    semaphore--
+    semaphore--;
     if (semaphore === 0) {
-      getTopBar(this.props).hide()
+      getTopBar(this.props).hide();
     }
   }
 
   render() {
-    return null
+    return null;
   }
 }
 
-export default TopBar
+export default TopBar;
